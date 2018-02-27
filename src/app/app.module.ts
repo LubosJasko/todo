a81a489d56e2e26ba24cb55dfbef2498bc868bfe
lubos.jasko <lubos.jasko@globallogic.com>
2018-02-27 08:31:09 +0100

    added basic routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,31 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {HttpClientModule} from '@angular/common/http';
-
+import {RouterModule, Routes} from "@angular/router";
 
 import {AppComponent} from './app.component';
 import {NotesComponent} from './components/notes/notes.component';
 import {DetailComponent} from './components/detail/detail.component';
 import {HeaderComponent} from './components/header/header.component';
 import {SharedService} from "./config/noteData.service";
+import {InfoComponent} from './components/info/info.component';
+
+const appRoutes: Routes = [
+  {path: 'info', component: InfoComponent}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     NotesComponent,
     DetailComponent,
-    HeaderComponent
+    HeaderComponent,
+    InfoComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [SharedService],
   bootstrap: [AppComponent]
